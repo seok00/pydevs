7a283e70d1c2cb820a8f19c3e80d9b0811f690ed
bullgom <bullgom0213@gmail.com>
2019-05-27 22:11:18 +0900

    Changed Message to have both source and destination ports

--- versions/1.1/Message.py
+++ versions/1.1/Message.py
@@ -1,89 +1,54 @@
 from Port import Port
 from Model import Model
-from Processor import Processor
+from enum import Enum
 
 class Message:
-    Done = "done"
-    Star = "star"
-    
-    class X(Message):
-        def __init__(self, source, time, portName, value=None):
-            """Message that represents external input to simulators
-            
-            Arguments:
-                source {[Model]} -- [Source of the message]
-                time {[float]} -- [Time when the message was created]
-                portName {[str]} -- [Name of source port]
-            
-            Keyword Arguments:
-                value {[any]} -- [Actual message that is to be passed] (default: {None})
-            """
-            super().__init__(source, time, portName, value)
-    
-    class Y(Message):
-        def __init__(self, source, time, portName, value=None):
-            """Message that represents external output from simulators
-            
-            Arguments:
-                source {[Model]} -- [Source of the message]
-                time {[float]} -- [Time when the message was created]
-                portName {[str]} -- [Name of source port]
-            
-            Keyword Arguments:
-                value {[any]} -- [Actual message that is to be passed] (default: {None})
-            """
-            super().__init__(source, time, portName, value)
-    
-    class Done(Message):
-        def __init__(self, source, value):
-            """Message that represents end of internal transition
-            
-            Arguments:
-                source {[Model]} -- [Source of the message]
-            
-            Keyword Arguments:
-                value {[any]} -- [Actual message that is to be passed] (default: {None})
-            """
-            assert type(value) is float
-            # value is time of next internal event
-            super().__init__(source, None, None, value)
-    
-    class Star(Message):
-        def __init__(self, portName):
-            """Message that represents start of internal transition
-            
-            Arguments:
-                portName {[str]} -- [Name of source port]
-            
-            Keyword Arguments:
-                value {[any]} -- [Actual message that is to be passed] (default: {None})
-            """
-            super().__init__(None, None, portName, Star)
+    class MsgType(Enum):
+        Done = "done"
+        Star = "star"
+        X = "x"
+        Y = "y"
 
-    def __init__(self, source, time, portName, value=None):
-        assert type(time) is float
-        self.port = Port(source, portName)
+    def __init__(self, msgType, srcPort, dstPort, time, value=None):
+        """[__init__]
+        Arguments:
+            msgType {[MsgType]} -- [Represents type of the message in DEVS formalism]
+            srcPort {[Port]} -- [Source port of the message]
+            dstPort {[Port]} -- [Destination port of the message]
+            time {[float]} -- [Time when the message was sent]
+        
+        Keyword Arguments:
+            value {[any]} -- [Value to be carried with the message] (default: {None})
+        """
+        self.msgType = msgType
+        self.srcPort = srcPort
+        self.dstPort = dstPort
         self.time = time
         self.value = value
-
-    @property
-    def port(self):
-        return self.port
-
-    @port.setter
-    def port(self, instance):
-        assert type(instance) is Port
-        self.port = instance
     
+    @property
+    def srcPort(self):
+        return self.srcPort
+
+    @srcPort.setter
+    def srcPort(self, instance):
+        assert isinstance(instance, Port)
+        self.srcPort = instance
+    
+    @property
+    def dstPort(self):
+        return self.dstPort
+
+    @dstPort.setter
+    def dstPort(self, instance):
+        assert isinstance(instance, Port)
+        self.dstPort = instance
+
     @property
     def time(self):
         return self.time
-
+    
     @time.setter
     def time(self, value):
-        assert type(time) is float
+        assert type(value) is float
         self.time = value
-
-
-
-
